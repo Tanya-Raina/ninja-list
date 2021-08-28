00e298c366ac3862dc8b429bd68f765d18970ad1
Tanya Raina <tanyaraina@Tanyas-MacBook-Pro.local>
2021-08-28 09:19:27 +0530

    Initial commit

---
+++ pages/about.js
@@ -0,0 +1,24 @@
+import Head from "next/head"
+
+const About = () => {
+    return (
+      <>
+        <Head>
+          <title>Ninja List | About</title>
+        </Head>
+        <div className="">
+          <h1>About</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
+      </>
+    );
+}
+ 
+export default About;
